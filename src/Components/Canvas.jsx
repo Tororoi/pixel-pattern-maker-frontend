2c37d4142746185e536b5b7e7d1dd858985128a1
Tororoi <cantwell.tom@gmail.com>
2020-05-27 16:36:01 -0400

    updated variable names for color replace

--- src/Components/Canvas.jsx
+++ src/Components/Canvas.jsx
@@ -14,23 +14,23 @@ class Canvas extends React.Component {
     }
 
     componentDidUpdate() {
-        const canvas = this.canvasRef.current  
-        const ctx = canvas.getContext('2d')
+        const cvs = this.canvasRef.current  
+        const ctx = cvs.getContext('2d')
         switch(this.props.canvasInfo.background) {
             case "white":
-                canvas.style.backgroundColor = "white"
+                cvs.style.backgroundColor = "white"
                 break;
             case "gray":
-                canvas.style.backgroundColor = "gray"
+                cvs.style.backgroundColor = "gray"
                 break;
             case "black":
-                canvas.style.backgroundColor = "black"
+                cvs.style.backgroundColor = "black"
                 break;
             case "transparent":
-                canvas.style.backgroundColor = "transparent"
+                cvs.style.backgroundColor = "transparent"
                 break;
             default:
-                canvas.style.backgroundColor = "white"
+                cvs.style.backgroundColor = "white"
         }
 
         if (this.props.canvasInfo.ctxClear === true) {
@@ -40,13 +40,13 @@ class Canvas extends React.Component {
         // change old color to new color
         const replaceColor = () => {
             //crate offscreen canvas with image to reduce size of array to iterate over.
-            let cvs = document.createElement('canvas');
-            cvs.width = this.props.canvasInfo.imageSize;
-            cvs.height = this.props.canvasInfo.imageSize;
-            const context = cvs.getContext('2d')
-            context.drawImage(this.canvasRef.current,0,0,256,256,0,0,this.props.canvasInfo.imageSize,this.props.canvasInfo.imageSize); // first four coords are the cropping area
+            let offScreenCanvas = document.createElement('canvas');
+            offScreenCanvas.width = this.props.canvasInfo.imageSize;
+            offScreenCanvas.height = this.props.canvasInfo.imageSize;
+            const offScreenCtx = offScreenCanvas.getContext('2d')
+            offScreenCtx.drawImage(this.canvasRef.current,0,0,256,256,0,0,this.props.canvasInfo.imageSize,this.props.canvasInfo.imageSize); // first four coords are the cropping area
 
-            const imageData = context.getImageData(0,0,this.props.canvasInfo.imageSize,this.props.canvasInfo.imageSize)
+            const imageData = offScreenCtx.getImageData(0,0,this.props.canvasInfo.imageSize,this.props.canvasInfo.imageSize)
 
             //color format helper functions
             const componentToHex = (c) => {
@@ -79,7 +79,7 @@ class Canvas extends React.Component {
             }
 
             // put the altered data back on the canvas  
-            context.putImageData(imageData,0,0);
+            offScreenCtx.putImageData(imageData,0,0);
             // clear main canvas
             ctx.clearRect(0,0,768,768)
             // restate imageSmoothing to prevent rare bug where image is rendered blurry
@@ -88,23 +88,19 @@ class Canvas extends React.Component {
             const b = this.props.canvasInfo.boxSize
             const squares = [{x: 0,y: 0},{x: b,y: 0},{x: b*2,y: 0},{x: 0,y: b},{x: b,y: b},{x: b*2,y: b},{x: 0,y: b*2},{x: b,y: b*2},{x: b*2,y: b*2}]
             squares.forEach(square => {
-                ctx.drawImage(cvs,square.x,square.y,b,b)
+                ctx.drawImage(offScreenCanvas,square.x,square.y,b,b)
             })
             this.props.replacingDispatch(false)
         }
 
         if (this.props.canvasInfo.replacing === true && this.props.canvasInfo.replacementAllowed === true) {replaceColor()}
-        // if (this.props.paletteInfo.pickerMouseDown) {replaceColor()}
         //Make object that allows access to indexes of pixels that need to be changed --- how?
-        //iterate through image data of a version of canvas at pixel scale 1 to reduce strain from function
-
-        // const imageData = ctx.getImageData(0,0,64,64)
-        // console.log(imageData)
+        
     }
 
     renderCanvas = () => {
-        const canvas = this.canvasRef.current        
-        const ctx = canvas.getContext('2d')
+        const cvs = this.canvasRef.current        
+        const ctx = cvs.getContext('2d')
         ctx.imageSmoothingEnabled = false;
         const b = this.props.canvasInfo.boxSize
         const squares = [{x: 0,y: 0},{x: b,y: 0},{x: b*2,y: 0},{x: 0,y: b},{x: b,y: b},{x: b*2,y: b},{x: 0,y: b*2},{x: b,y: b*2},{x: b*2,y: b*2}]
